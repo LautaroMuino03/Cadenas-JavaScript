0d62db8e750af7e5380cfdf240616fc314067178
Lautaro Muiño <117776336+LautaroMuino03@users.noreply.github.com>
2023-02-16 11:14:26 -0300

    Add files via upload

---
+++ cadenas.js
@@ -0,0 +1,25 @@
+let nombre = "Lautaro"
+console.log(nombre);
+let apellido = " Muiño"
+console.log(apellido);
+let estudiante = " Lautaro Muiño "
+console.log(nombre.concat(apellido));
+let estudianteMayus = "estudiante"
+console.log(estudianteMayus.toUpperCase())
+let estudianteMinus = "estudiante"
+console.log(estudianteMinus.toLowerCase())
+
+console.log(estudiante.length);
+
+console.log(nombre.charAt(0));
+console.log(apellido.charAt(5));
+
+console.log(estudiante.length);
+console.log(estudiante.trimStart().length);
+console.log(estudiante.trimEnd().length);
+
+
+var boolean = "¿el nombre se encuentra en la cadena estudiante?"
+boolean = "true"
+console.log(boolean);
+
